711189146464a7763c64f122f26ae3ee125fb339
FireUnderTheMountain <tisabelle.defoe@gmail.com>
2021-05-27 18:57:54 -0700

    Packaging script A packaging script that creates release zips for github

---
+++ package.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+which realpath >/dev/null || exit 2
+which zip >/dev/null || exit 3
+
+d="$(date +%F)"
+cd "$(realpath "${0%/*}")"/..
+zip -9 -ru Hiui-"$d".zip Hiui -x "Hiui/units/*" -x "Hiui/disabled/*" -x "Hiui/Textures/256x256 scratchboard.pdn" -x "Hiui/Textures/topbar*" -x "Hiui/hiui.code-workspace" -x "Hiui/.git/*" -x "Hiui/.vscode/*" -x "Hiui/.gitignore" -x "Hiui/package.sh"
