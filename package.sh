573857a540a39c140ea58b89fe35c187fed13b88
FireUnderTheMountain <tisabelle.defoe@gmail.com>
2021-06-26 23:10:59 -0700

    exclude README and focus texture from releases

--- package.sh
+++ package.sh
@@ -5,4 +5,4 @@ which zip >/dev/null || exit 3
 
 d="$(date +%F)"
 cd "$(realpath "${0%/*}")"/..
-zip -9 -ru Hiui-"$d".zip Hiui -x "Hiui/units/*" -x "Hiui/disabled/*" -x "Hiui/Textures/256x256 scratchboard.pdn" -x "Hiui/Textures/topbar*" -x "Hiui/hiui.code-workspace" -x "Hiui/.git/*" -x "Hiui/.vscode/*" -x "Hiui/.gitignore" -x "Hiui/package.sh"
+zip -9 -ru Hiui-"$d".zip Hiui -x "Hiui/units/*" -x "Hiui/disabled/*" -x "Hiui/Textures/256x256 scratchboard.pdn" -x "Hiui/Textures/focus*" -x "Hiui/Textures/topbar*" -x "Hiui/hiui.code-workspace" -x "Hiui/.git/*" -x "Hiui/.vscode/*" -x "Hiui/.gitignore" -x "Hiui/README.md" -x "Hiui/package.sh"
