0510cec4276cf8922fbe6d80a4b7d779734a7532
FireUnderTheMountain <tisabelle.defoe@gmail.com>
2022-04-07 17:19:24 -0700

    myslot support

---
+++ modules/myslot.lua
@@ -0,0 +1,178 @@
+--[[    Header
+In this opening block, only the name of the addon, version, info and dependencies needs to be changed.
+Dependencies are mod names that you require to be enabled for this mod to load. You should use the folder name exactly.
+Module information will be displayed on the main hiui page.
+The version is used to perform automatic initialization, and should be updated everytime you need first-time init to run again.
+--]]
+local Hiui = LibStub("AceAddon-3.0"):GetAddon("hiUI")
+local name, version = "MySlot Reminder", 0
+local mod = Hiui:NewModule(name, "AceEvent-3.0", "AceConsole-3.0")
+mod.modName, mod.version = name, version
+mod.info = "Reminds you to make MySlot backups of your action bars."
+mod.depends = {}
+
+--[[ Imports --]]
+-- local GlobalUIElement = _G["GlobalUIElement"]
+
+--[[    Database Access
+Store all of this module's variables under "global", "profile", or "char" respectively. These are shortcuts to their long forms:
+mod.db.global, mod.db.profile, mod.db.char
+--]]
+local global, profile, char
+
+--[[    Default Values
+You should include at least the following:
+defaults.global.debug = false
+defaults.profile.enabled = false
+defaults.char.initialized = false
+--]]
+local defaults = {
+    global = {
+        debug = true, -- noisy debugging
+    },
+    profile = {
+        enabled = false, -- have root addon enable?
+    },
+    char = {
+        initialized = 0, -- used for first time load
+        sample_function_run = false, -- alternative way to
+        sample_func_two_run = false, -- do first time load
+    },
+}
+
+
+--[[    "Features" local variable
+Define all your custom functions that are used by the UI in here. This makes them available to run at once during initialization.
+Remember to set/change db values during these functions.
+--]]
+local features = {
+	sample_function = function(info)
+        char.sample_function_run = true
+        return
+	end,
+    sample_func_two = function(info, value)
+        if value then -- running as "set"
+            char.sample_func_two_run = value
+        else -- running as "get"
+            return char.sample_func_two_run
+        end
+    end,
+}
+
+--[[    GUI Options Menu
+Only edit the tables under args. Leave "enable" and "disabledWarning" alone.
+The name of each option is what you type to enable/disable it, so make them keyboard friendly. Ex. "exampleoption" instead of "example_option"
+--]]
+local options = {
+    name = " " .. name,
+    type = "group",
+    args = {
+        enable = {
+            order = 0,
+            name = "Enable " .. name,
+            desc = "Check to enable this module.",
+            type = "toggle",
+            set = function(_, value)
+                profile.enabled = value
+                if value then
+                    mod:Enable()
+                else
+                    mod:Disable()
+                end
+            end,
+            get = function() return profile.enabled end,
+        },
+        debug = {
+            order = 1,
+            name = "Noisy debugging",
+            desc = "Print lots of text to the chatbox.",
+            type = "toggle",
+            set = function(_, value) global.debug = value end,
+            get = function() return global.debug end,
+        },
+        disabledWarning = {
+            order = 2,
+            name = "Disabled! None of the options will function until you enable it again.",
+            type = "description",
+            width = "full",
+        },
+        exampleoption = {
+			name = "Function",
+			desc = "Run a function?",
+			type = "execute",
+			confirm = true,
+			func = features.sample_function,
+		},
+        exampleoptiontwo = {
+            name = "Another function",
+            desc = "Run a function on checking.",
+            type = "toggle",
+            set = features.sample_func_two,
+            get = features.sample_func_two,
+        },
+    },
+}
+
+--[[    Option Page Show
+Removes the disabled state of each option. Also hides the disabled warning. Do not modify.
+--]]
+local function enableArgs(optionsTable)
+    for k, v in pairs(optionsTable.args) do
+        if v.disabled then v.disabled = false end
+    end
+    optionsTable.args.disabledWarning.hidden = true
+end
+
+--[[    Option Page Hide
+Grays out the options when you disable the addon. Also hides the disabled warning. Do not modify.
+--]]
+local function disableArgs(optionsTable)
+    for k, v in pairs(optionsTable.args) do
+        if k ~= "enable" and k ~= "disabledWarning" then
+            v.disabled = true
+        end
+    end
+    optionsTable.args.disabledWarning.hidden = false
+end
+
+function mod:OnInitialize()
+    --[[ data initialization. do not modify. --]]
+    self.db = Hiui.db:RegisterNamespace(name, defaults)
+    global, profile, char = self.db.global, self.db.profile, self.db.char
+
+    --[[ option page initialization. do not modify. --]]
+    LibStub("AceConfig-3.0"):RegisterOptionsTable(name, options);
+    local parentOptions = tostring(Hiui.optionsName)
+    Hiui.optionFrames[name] = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(name, options.name, parentOptions)
+
+    --[[ Gray out args. do not modify. --]]
+    if not profile.enabled then disableArgs(options) end
+
+    --[[ Module specific on-load routines go here. --]]
+end
+
+function mod:OnEnable()
+    --[[ For combat-unsafe mods. --]]
+    if InCombatLockdown() then
+        return self:RegisterEvent("PLAYER_REGEN_ENABLED", "OnEnable")
+    end
+    self:UnregisterEvent("PLAYER_REGEN_ENABLED")
+
+    enableArgs(options) -- do not remove.
+
+    --[[ First time enablement, run if we've updated this module. You can modify this according to the module's needs. --]]
+	if char.initialized < mod.version then
+		for _, feature in pairs(features) do
+			feature()
+		end
+		char.initialized = mod.version
+	end
+
+    --[[ Module specific on-run routines go here. --]]
+end
+
+function mod:OnDisable()
+    disableArgs(options) -- do not remove.
+
+    --[[ Module specific on-disable routines go here. --]]
+end
